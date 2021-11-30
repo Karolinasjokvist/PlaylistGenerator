763f993f7c25e6d8e70af1f00148e3f76897fb44
Karolinasjokvist <83401567+Karolinasjokvist@users.noreply.github.com>
2021-11-30 13:05:26 +0100

    commit

--- src/js/songSource.js
+++ src/js/songSource.js
@@ -2,21 +2,16 @@ import {BASE_URL,API_KEY} from './apiConfig';
 
 export const SongSource = {   // JS object creation literal
     apiCall(params) {
-        return fetch(BASE_URL + params, {
-            "method": "GET",
-            "headers": {
-                'x-rapidapi-host': 'deezerdevs-deezer.p.rapidapi.com',
-                'x-rapidapi-key': API_KEY
-            }
-        }).then(response => { if (response.status != 200) throw "error " + response.status; return response })
+        return console.log(BASE_URL + params),
+        fetch(BASE_URL + params).then(response => { if (response.status != 200) throw "error " + response.status; return response })
 
         // from HTTP response headers to HTTP response data
         .then(response => response.json());
+        
     }
     ,   // comma between object entries
     searchSongs(params) {
-        return SongSource.apiCall('/album/' + params
-            + new URLSearchParams(params))
+        return this.apiCall('/album/' + params)
             .then(data => { return data.results });
     },
 
