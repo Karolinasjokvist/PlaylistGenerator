4b5600ffd0d4cdc1d426c187b2c45b9a89a1e7d6
Karolinasjokvist <83401567+Karolinasjokvist@users.noreply.github.com>
2021-12-01 17:34:45 +0100

    update

--- src/js/views/exploreGenresView.js
+++ src/js/views/exploreGenresView.js
@@ -2,11 +2,11 @@ import React, { Component } from 'react';
 export function ExploreGenresView(props) {
     return (
         
-        <div>
+        <div className="explorePage">
             {props.genres.map(genre =>
-                <span class="genresView" key={genre.id}   >
-                    <img src={genre.picture} class="genreImgView"></img>
-                    <div>{genre.name}</div>
+                <span className="exploreView" key={genre.id}   >
+                    <img src={"https://images.freeimages.com/images/large-previews/056/concert-1189401.jpg"} className="exploreImg"></img>
+                    <div className="exploreText">{genre.name}</div>
                 </span>
             )}
         </div>
