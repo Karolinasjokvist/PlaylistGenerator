bfd6c4784aa65d44a83195a28afc81daa9a19fd0
Karolinasjokvist <83401567+Karolinasjokvist@users.noreply.github.com>
2021-12-01 20:20:40 +0100

    update

--- src/App.js
+++ src/App.js
@@ -6,29 +6,34 @@ import { ExploreArtistsView} from './js/views/exploreArtistsView';
 import { GenerateStartView} from './js/views/generateStartView';
 import { SongSource } from './js/songSource';
 import { ExploreGenresView } from './js/views/exploreGenresView';
-import { ExploreGenresPresenter } from './js/exploreGenresPresenter';
+import { ExploreArtistsPresenter, ExploreGenresPresenter } from './js/exploreArtistsPresenter';
 import {PlaylistModel} from './js/playlistModel';
+import { Genres } from './js/genres';
 
 
 function App(props) {
   const model = new PlaylistModel("hej",6);
-  const genres = ["106","165","113"]
+  const genres = new Genres();
   return (
     <div>
       {TopbarView()}
-      {console.log(model.numberOfSongs["10"])}
-      {/*{<ExploreGenresPresenter genres = {genres}/>}
-       {StartView()} /}
-      {/ {console.log(SongSource.apiCall("/album/302127"))} /}
-      {/ {model.addGenreToPlaylist("106", 3)} /}
-      {/ {model.addGenreToPlaylist("106", 5)} /}
-      {/ {console.log(model.genres["106"])} */}
+      {console.log(genres.list)}
+      {<ExploreGenresView genres = {genres.list}
+           genreChosen = {(id) => console.log(id)}/>}
+
+      {<ExploreArtistsPresenter genre = {genres.list[1].id}/>}
+      {/* {console.log(model.numberOfSongs["10"])} */}
+      {/* {<ExploreGenresPresenter />} */}
+       {/* {StartView()} /} */}
+      {/* {/ {console.log(SongSource.apiCall("/album/302127"))} /} */}
+      {/* {/ {model.addGenreToPlaylist("106", 5)} /} */}
+      {/* {/ {console.log(model.genres["106"])} */}
       {/* {StartView()}*/}
-      {GenerateStartView(model)}
-      {console.log(SongSource.apiCall("/album/302127"))}
-      {model.addGenreToPlaylist("106", 3)}
-      {model.addGenreToPlaylist("106", 5)}
-      {console.log(model.genres["106"])}
+      {/* {GenerateStartView(model)} */}
+      {/* {console.log(SongSource.apiCall("/album/302127"))} */}
+      {/* {model.addGenreToPlaylist("106", 3)} */}
+      {/* {model.addGenreToPlaylist("106", 5)} */}
+      {/* {console.log(model.genres["106"])} */}
     </div>
   );
 }
--- src/js/views/exploreGenresView.js
+++ src/js/views/exploreGenresView.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
+import '../../css/explore.css';
+
 export function ExploreGenresView(props) {
     return (
-        
         <div className="explorePage">
             {props.genres.map(genre =>
-                <span className="exploreView" key={genre.id}   >
-                    <img src={"https://images.freeimages.com/images/large-previews/056/concert-1189401.jpg"} className="exploreImg"></img>
-                    <div className="exploreText">{genre.name}</div>
-                </span>
-            )}
+                    <span className="exploreView" key={genre.id} onClick ={e => props.genreChosen(e = genre.id)}  >
+                        <img src={genre.img} className="exploreImg"></img>
+                        <div>{genre.name}</div>
+                    </span>
+                )}
         </div>
     );
 }
