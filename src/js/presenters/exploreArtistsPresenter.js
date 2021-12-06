1219e4ea364595eb588f884dafc3e82dce3a00da
Karolinasjokvist <83401567+Karolinasjokvist@users.noreply.github.com>
2021-12-06 10:37:30 +0100

    update

--- src/js/presenters/exploreArtistsPresenter.js
+++ src/js/presenters/exploreArtistsPresenter.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react';
 import { SongSource } from '../songSource';
 import PromiseNoData from '../promiseNoData';
-import ExploreArtistsView from '../views/exploreArtistsView';
+import { ExploreArtistsView } from '../views/exploreArtistsView';
+import { ExplorePlayMusic } from '../views/exploreArtistsView';
 
 function ExploreArtistsPresenter(props) {
     const [promise, setPromise] = React.useState(null);
     const [data, setData] = React.useState(props.model.currentGenreArtists);
     const [error, setError] = React.useState(props.model.currentGenreError);
 
-    // const [promiseSongs, setPromiseSongs] = React.useState(null);
-    // const [dataSongs, setDataSongs] = React.useState(null);
+    const [promiseSongs, setPromiseSongs] = React.useState(null);
+    const [dataSongs, setDataSongs] = React.useState(null);
+    const [errorSongs, setErrorSongs] = React.useState(null);
 
 
     React.useEffect(() => {
@@ -26,15 +28,18 @@ function ExploreArtistsPresenter(props) {
 
     return (
         <div>
-            {console.log(data)}
+            {/* {console.log(data)} */}
             {PromiseNoData(promise, data, error) || (
                 <ExploreArtistsView artist={data.data}
-                // playAudio={(id) => setPromiseSongs(
-                // SongSource.getSongsFromArtist(id)
-                // .then((data) => setDataSongs(data))
-                // .catch((error) => setError(error))
-
-                />
+                    func={(id) => {
+                        console.log(id)
+                        setPromiseSongs(
+                            SongSource.getSongsFromArtist(id)
+                                .then((dataSongs) => {setDataSongs(dataSongs); <ExplorePlayMusic songs={dataSongs.data}/>})
+                                .catch((errorSongs) => setError(errorSongs)),
+                                console.log(dataSongs.data),
+                        )
+                    }} />
             )}
         </div>
     );
--- src/js/views/exploreArtistsView.js
+++ src/js/views/exploreArtistsView.js
@@ -3,21 +3,31 @@ import '../../css/explore.css';
 
 function ExploreArtistsView(props) {
     return (
-        <div className="explorePage" /*onClick={e => playAudio(props.artist.id)}*/>
+        <div className="explorePage" >
             <p className="title">Explore artists in the {props.artist.genre} genre!</p>
-            {props.artist.slice(0,15).map(artist =>
-                    <span className="exploreView" key={artist.id}  >
-                        <img src={artist.picture_xl} className="exploreImgArtist"></img>
-                        <div>{artist.name}</div>
-                    </span>
-                )}
+            {props.artist.slice(0, 15).map(artist =>
+                <span className="exploreView" key={artist.id} onClick={e => props.func(artist.id)} >
+                    <img src={artist.picture_xl} className="exploreImgArtist"></img>
+                    <div>{artist.name}</div>
+                </span>
+            )}
         </div>
     );
 }
 
-// function playAudio(){
-    // new Audio("https://cdns-preview-c.dzcdn.net/stream/c-cca63b2c92773d54e61c5b4d17695bd2-8.mp3").play();
-//    }
+function ExplorePlayMusic(props){
+    return (
+        <div>
+            {console.log("hej")}
+        {props.songs.slice(0, 2).map(song =>{console.log(song.preview)})}
+        </div>
+    )
+}
+
+function playAudio(song) {
+    console.log("hej")
+    new Audio(song).play();
+}
 
 
-export default ExploreArtistsView;
+export {ExploreArtistsView, ExplorePlayMusic};
