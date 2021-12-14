3aae3d60a09f48e54ba61a7383b4ded289a2ca72
Nemanja Jancic <nemanja.jancic@gmail.com>
2021-12-14 17:09:17 +0100

    update
    topbar, start mobile

--- src/js/views/topbarView.js
+++ src/js/views/topbarView.js
@@ -6,8 +6,8 @@ function TopbarView() {
         <div className="topBar">
             <div className="top">
                 <div className="topbuttons">
-                    <button className="b1" onClick={e => { window.location.hash = "#register" }}>Register</button>
-                    <button className="b1" onClick={e => { window.location.hash = "#login" }}>Log in</button>
+                    <button className="b1 register" onClick={e => { window.location.hash = "#register" }}>Register</button>
+                    <button className="b1 login" onClick={e => { window.location.hash = "#login" }}>Log in</button>
                 </div>
             </div>
             <div className="head">PlaylistGenerator</div>
